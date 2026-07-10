dbef667e5c43e1ec4225dbd2effd1b1543c340e5
Sonia Messadi <sonyamessaadi499@gmail.com>
2026-07-11 01:12:49 +0200

    Fallo forzado en prueba unitaria

--- src/main/java/com/lauracercas/moviecards/model/Actor.java
+++ src/main/java/com/lauracercas/moviecards/model/Actor.java
@@ -44,9 +44,9 @@ public class Actor {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { 
+     return “Hola”; 
+    } 
 
     public void setName(String name) {
         this.name = name;
