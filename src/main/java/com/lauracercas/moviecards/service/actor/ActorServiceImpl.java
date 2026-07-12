623ee234d90c9ca5bc1caf65e801efcca68db8c3
Sonia Messadi <sonyamessaadi499@gmail.com>
2026-07-12 13:48:15 +0200

    Añadida fecha de fallecimiento de actores

--- src/main/java/com/lauracercas/moviecards/service/actor/ActorServiceImpl.java
+++ src/main/java/com/lauracercas/moviecards/service/actor/ActorServiceImpl.java
@@ -2,7 +2,9 @@ package com.lauracercas.moviecards.service.actor;
 
 
 import com.lauracercas.moviecards.model.Actor;
-import com.lauracercas.moviecards.repositories.ActorJPA;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,24 +17,36 @@ import java.util.List;
 @Service
 public class ActorServiceImpl implements ActorService {
 
-    private final ActorJPA actorJPA;
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    public ActorServiceImpl(ActorJPA actorJPA) {
-        this.actorJPA = actorJPA;
-    }
+    @Value("${moviecards.service.url}")
+    private String serviceUrl;
+
+    
 
     @Override
     public List<Actor> getAllActors() {
-        return actorJPA.findAll();
+    ResponseEntity<Actor[]> response =
+            restTemplate.getForEntity(serviceUrl + "/actors", Actor[].class);
+
+    return List.of(response.getBody());
+}
+@Override
+public Actor save(Actor actor) {
+
+    if (actor.getId() == null) {
+        restTemplate.postForObject(serviceUrl + "/actors", actor, Actor.class);
+    } else {
+        restTemplate.put(serviceUrl + "/actors", actor);
     }
 
-    @Override
-    public Actor save(Actor actor) {
-        return actorJPA.save(actor);
-    }
+    return actor;
+}
 
     @Override
-    public Actor getActorById(Integer actorId) {
-        return actorJPA.getById(actorId);
-    }
+public Actor getActorById(Integer actorId) {
+    return restTemplate.getForObject(
+            serviceUrl + "/actors/" + actorId,
+            Actor.class);
+}
 }
